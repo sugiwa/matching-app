f45eac432819f08385618b45ce9900e3db1a6a27
sugiwa <sugi.warra@gmail.com>
2023-10-21 01:15:52 +0900

    fix GroupList style

--- src/app/components/Group/GroupList.tsx
+++ src/app/components/Group/GroupList.tsx
@@ -1,4 +1,4 @@
-import { Box } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import GroupCard from "./GroupCard";
 
 const groupList = [
@@ -18,18 +18,25 @@ const groupList = [
 
 const GroupList = () => {
   return (
-    <Box
-      sx={{
-        padding: "0 20px",
-        display: "flex",
-        flex: "1",
-        backgroundColor: "#DDFFFF",
-        border: "1px #EEEEEE solid",
-      }}
-    >
-      {groupList.map((group, i) => (
-        <GroupCard key={i} name={group.name} description={group.description} />
-      ))}
+    <Box sx={{ margin: "0 40px" }}>
+      <Typography>GroupList</Typography>
+      <Box
+        sx={{
+          padding: "0 20px",
+          display: "flex",
+          flex: "1",
+          backgroundColor: "#DDFFFF",
+          border: "1px #EEEEEE solid",
+        }}
+      >
+        {groupList.map((group, i) => (
+          <GroupCard
+            key={i}
+            name={group.name}
+            description={group.description}
+          />
+        ))}
+      </Box>
     </Box>
   );
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,17 +2,21 @@ import Image from "next/image";
 import Header from "./components/Header";
 import NavBar from "./components/NavBar";
 import GroupList from "./components/Group/GroupList";
+import { Box, Container, Grid } from "@mui/material";
 
 export default function Home() {
   return (
-    <div className="h-screen flex flex-col">
+    <Box sx={{ height: "100vh", display: "flex", flexDirection: "column" }}>
       <Header />
-      <div className="flex flex-1">
+
+      <Box sx={{ display: "flex", flex: 1 }}>
         <NavBar />
-        <main>
-          <GroupList />
-        </main>
-      </div>
-    </div>
+        <Container sx={{ margin: "20px 0", display: "flex", flex: 1, flexDirection: "column" }}>
+          <Grid xs={12} sx={{ width: "100%", margin: "10px 0" }}>
+            <GroupList />
+          </Grid>
+        </Container>
+      </Box>
+    </Box>
   );
 }
