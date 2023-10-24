4afa55d3293c17f6837f3d201da2817289a61238
sugiwa <sugi.warra@gmail.com>
2023-10-25 00:23:02 +0900

    feat OAuth

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,22 @@
+import NextAuth from 'next-auth/next';
+import GoogleProvider from 'next-auth/providers/google';
+import GithubProvider from 'next-auth/providers/github';
+import { NextAuthOptions } from 'next-auth';
+
+const option: NextAuthOptions = {
+  debug: true,
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
+    // GoogleProvider({
+    //   clientId: process.env.GOOGLE_CLIENT_ID,
+    //   clientSecret: process.env.GOOGLE_SECRET,
+    // }),
+  ],
+};
+
+const handler = NextAuth(option);
+
+export { handler as GET, handler as POST };
